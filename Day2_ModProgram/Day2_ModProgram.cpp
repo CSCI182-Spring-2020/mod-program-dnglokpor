2b05601e312286a1206608300f96c8973ef91556
dnglokpor <59987038+dnglokpor@users.noreply.github.com>
2020-02-04 23:03:17 -0600

    Added a timer to test compare Mod and Mod2

--- Day2_ModProgram/Day2_ModProgram.cpp
+++ Day2_ModProgram/Day2_ModProgram.cpp
@@ -1,25 +1,12 @@
+#include <Windows.h>
+#include <ctime>
 
-/*
-CSCI 182 - Asssignment 1
-Day2_ModProgram.cpp
-Manually crate a Mod funtion for use
+CONST int NO_ITERATIONS = 10000;
 
-Brett Huffman
-v1.0 1/29/2020
-*/
+typedef long long int64; typedef unsigned long long uint64;
 
 #include <iostream>
-
-// Get the min value of two input values
-int GetMinValue(int valueA, int valueB)
-{
-	if (valueA < valueB) { // valueA is the min
-		return valueA;
-	}
-	else { // valueB is min or equal to valueA
-		return valueB;
-	}
-}
+using namespace std;
 
 /// <summary>
 /// Returns the remainder of the integer division of two numbers by division then substraction
@@ -28,7 +15,7 @@ int GetMinValue(int valueA, int valueB)
 /// <param name="valueA"></param>
 /// <param name="valueB"></param>
 /// <returns></returns>
-int Mod (int valueA, int valueB) {
+int Mod(int valueA, int valueB) {
 	int valueC = 0, mod = 0;
 
 	if (valueB != 0) {
@@ -51,7 +38,7 @@ int Mod (int valueA, int valueB) {
 /// <returns></returns>
 int Mod2(int valueA, int valueB) {
 	int mod = valueA; // starts at valueA
-	
+
 	if (valueB != 0) {
 		while (mod >= valueB) {
 			mod -= valueB; // continuously substract valueB of valueA until we can't anymore
@@ -65,35 +52,74 @@ int Mod2(int valueA, int valueB) {
 	}
 }
 
-int main ()
+
+uint64 GetTimeMs64()
 {
-	// std::cout << "Hello World!\n";
-	int valueA = 400;
-	int valueB = 220;
+	FILETIME ft;
+	LARGE_INTEGER li;
+	GetSystemTimeAsFileTime(&ft);
+	li.LowPart = ft.dwLowDateTime;
+	li.HighPart = ft.dwHighDateTime;
 
-	// Find the min value and pretty print it to the screen
-	// std::cout << "Min Value: " << GetMinValue(a, b) << "\n";
+	uint64 ret = li.QuadPart;
+	ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+	ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
 
-	// Print the modulus of the a / b
-	if(Mod(valueA, valueB) == -1)
-		std::cout << "Can't divide by 0" << "\n";
-	else
-		std::cout << "Using Mod, " << valueA << "/" << valueB << " has a remainder of " << Mod(valueA, valueB) << ".\n";
-	
-	// Print the modulus of the a / b using the 2nd method
-	if (Mod2(valueA, valueB) == -1)
-		std::cout << "Can't divide by 0" << "\n";
-	else
-		std::cout << "Using Mod2, " << valueA << "/" << valueB << " has a remainder of " << Mod2(valueA, valueB) << ".\n";
+	return ret;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	double firstNo = 0.0;
+	double secondNo = 0.0;
+	double resultSubMethod = 0.0;
+	double resultDivMethod = 0.0;
+	uint64 startTime = 0.0;
+	uint64 endTime = 0.0;
+	uint64 nTickCountFunction1 = 0.0;
+	uint64 nTickCountFunction2 = 0.0;
+
+	std::srand(199); // use as seed for random generator
+
+	startTime = GetTimeMs64();
+
+	for (int i = 0; i < NO_ITERATIONS; i++)
+	{
+		// if (i % 10000 == 0) cout << i << endl;
+
+		// Get random numbers between 1 - 1001 (never want 0)
+		firstNo = std::rand() % 1000 + 1;
+		secondNo = std::rand() % 1000 + 1;
+
+		// PUT THE CALL TO YOUR FIRST MOD FUNCTION HERE
+		Mod(firstNo, secondNo);
+	}
+	// Get the End time + add it to our counter
+	endTime = GetTimeMs64();
+	nTickCountFunction1 = endTime - startTime;
+
+
+	std::srand(199); // use as seed for random generator
+	startTime = GetTimeMs64();
+	for (int i = 0; i < NO_ITERATIONS; i++)
+	{
+		// if (i % 10000 == 0) cout << i << endl;
+
+		// Get random numbers between 1 - 1001 (never want 0)
+		firstNo = std::rand() % 1000 + 1;
+		secondNo = std::rand() % 1000 + 1;
+
+		// PUT THE CALL TO YOUR SECOND MOD FUNCTION HERE
+		Mod2(firstNo, secondNo);
+	}
+
+	endTime = GetTimeMs64();
+	nTickCountFunction2 = endTime - startTime;
+
+	std::cout << "\t\tMethod 1" << "\tMethod 2" << "\n";
+	std::cout << "Total Count:\t" << NO_ITERATIONS << "\t\t" << NO_ITERATIONS << "\n";
+	std::cout << "Total Time:\t" << nTickCountFunction1 << "ms\t\t" << nTickCountFunction2 << "ms\n";
+
+	return 0;
+}
